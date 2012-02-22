e7d698e30e0085bdd1464b25d358aff868b28e66
Tom OB <tobrien999@gmail.com>
2012-02-22 11:31:27 -0700

    added mailer and changed layout

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,25 @@
 class ContactsController < ApplicationController
   # GET /contacts
   # GET /contacts.json
-  def index
-    @contacts = Contact.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @contacts }
-    end
-  end
-
-  # GET /contacts/1
-  # GET /contacts/1.json
-  def show
-    @contact = Contact.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @contact }
-    end
-  end
+  # def index
+  #   @contacts = Contact.all
+  # 
+  #   respond_to do |format|
+  #     format.html # index.html.erb
+  #     format.json { render json: @contacts }
+  #   end
+  # end
+  # 
+  # # GET /contacts/1
+  # # GET /contacts/1.json
+  # def show
+  #   @contact = Contact.find(params[:id])
+  # 
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.json { render json: @contact }
+  #   end
+  # end
 
   # GET /contacts/new
   # GET /contacts/new.json
